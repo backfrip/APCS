622d6597f478be9cd150d2b8842d2a4a651598a0
E Stringham <backfrip@users.noreply.github.com>
2014-11-18 17:38:25 -0500

    Played around with Sublime Text- doesn't seem terribly compatable with files created (and indented, esp.) in emacs

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -2,47 +2,52 @@ import java.util.*;
 
 public class ArrayListMethods {
 
-    public static void collapseDuplicates(ArrayList<Integer> L) {
-	for (int i = 0; i < L.size()-1; i++) {
-	    if ((int)L.get(i) == (int)L.get(i+1)) {
-		L.remove(i+1);
-		i--;
-	    }
+	public static void collapseDuplicates(ArrayList<Integer> L) {
+		for (int i = 0; i < L.size()-1; i++) {
+			if ((int)L.get(i) == (int)L.get(i+1)) {
+				L.remove(i+1);
+				i--;
+			}
+		}
 	}
-    }
 
-    public static void randomize(ArrayList<Integer> L) {
-	Random randy = new Random();
-	for (int i = L.size()-1; i >= 0; i--) {
-	    L.add(L.remove(randy.nextInt(i+1)));
+	/*
+		Turns out, doesn't really save on memory (due to individual
+		command memory usage and whatnot)
+	*/
+	public static void randomize(ArrayList<Integer> L) {
+		Random randy = new Random();
+		for (int i = L.size()-1; i >= 0; i--) {
+			L.add(L.remove(randy.nextInt(i+1)));
+		}
 	}
-    }
 
-    public static void main(String[]args) {
+	public static void main(String[]args) {
 
-	ArrayList<Integer> L = new ArrayList<Integer>();
-	L.add(new Integer(3));
-	L.add(new Integer(4));
-	L.add(new Integer(4));
-	L.add(new Integer(2));
-	L.add(new Integer(2));
-	L.add(new Integer(1));
-	L.add(new Integer(2));
+		ArrayList<Integer> L = new ArrayList<Integer>();
+		L.add(new Integer(3));
+		L.add(new Integer(4));
+		L.add(new Integer(4));
+		L.add(new Integer(2));
+		L.add(new Integer(2));
+		L.add(new Integer(1));
+		L.add(new Integer(2));
 
-	collapseDuplicates(L);
-	System.out.println(L);
+		collapseDuplicates(L);
+		System.out.println(L);
 
-	System.out.println();
-	ArrayList<Integer> M = new ArrayList<Integer>();
-	for (int i = 1; i < 21; i++) {
-	    M.add(i);
+		System.out.println();
+		ArrayList<Integer> M = new ArrayList<Integer>();
+		for (int i = 1; i < 21; i++) {
+			M.add(i);
+		}
+
+		System.out.println(M);
+		System.out.println();
+
+		randomize(M);
+		System.out.println(M);
+
 	}
-	System.out.println(M);
-	System.out.println();
-
-	randomize(M);
-	System.out.println(M);
-
-    }
 
 }
