66c809b3d7f10ec067dd9582dbbeb2e6c7194cec
E Stringham <backfrip@users.noreply.github.com>
2014-11-23 17:22:15 -0500

    Added scanning of " words.txt\

--- 03WordSearch/Test.java
+++ 03WordSearch/Test.java
@@ -1,21 +1,11 @@
+import java.io.IOException;
+
 public class Test {
 
-	// Note to self: make Sublime Text indent with spaces for emacs consitency
+    public static void main(String[] args) throws IOException {
 
-	public static void main(String[]args) {
-		WordGrid bord = new WordGrid(4,4);
-		System.out.println(bord.addWordHorizontal("PING",0,0));
-		System.out.println(bord.addWordHorizontal("PONG",1,0));
-		System.out.println(bord.addWordHorizontal("ING",2,1));
-		System.out.println(bord.addWordHorizontal("SIN",2,0));
-		System.out.println(bord.addWordHorizontal("GROUCHOMARX",3,0));
-		System.out.println(bord.addWordVertical("GGG",0,3));
-		System.out.println(bord.addWordVertical("GGGA",3,0));
-		System.out.println(bord.addWordVertical("PPSS",0,0));
-		System.out.println(bord.addWordDiagonal("PONG",0,0));
-		System.out.println(bord.addWordDiagonal("PIN",1,0));
-		System.out.println(bord.addWordDiagonal("SO",2,0));
+	WordGrid board = new WordGrid(10, 10);
+	
+    }
 
-		System.out.println("\n"+bord);
-	}
 }
