f81f876deabb32b9e7ae211debec316089be786b
E Stringham <backfrip@users.noreply.github.com>
2014-11-26 23:44:00 -0500

    Formatting adjustments made while fiddling with Eclipse versions, I think. Man I hate that it won't close tabs on exiting. Like, it's an incredibly insignificant feature, but it just reeeeally annoys me that it won't do it. (And also the fact that it mentions this feature in the documentation starting with Juno but I tested the versions all the way back to Indigo and none of them do it!!!)

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -1,8 +1,8 @@
 public class Driver {
-    public static void main(String[]args) {
-	// int rows = args[0];
-	// int cols = args[1];
-	// int randomSeed = args[2];
-	// int answers = args[3];
-    }
+	public static void main(String[] args) {
+		// int rows = args[0];
+		// int cols = args[1];
+		// int randomSeed = args[2];
+		// int answers = args[3];
+	}
 }
